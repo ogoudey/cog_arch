7b80a22ae9966f4fba1c0d74a1098e5075c19a0d
Olin <olin.goog@gmail.com>
2024-12-23 13:36:34 -0500

    Mock simple plan

--- sound_interface.py
+++ sound_interface.py
@@ -4,12 +4,25 @@ import threading
 
 # Should have audio.py here...
 
-def listen_for_hl(hl_decoder):
-    exhaust = hl_decoder.run()
-    if exhaust:
-        print("Stopping listening due to: " + exhaust + " detected in: " + "".join(map(str, hl_decoder.composition.values())))
-    else:
-        print("How am I here?")
+
+
+class SoundInterface:
+    def __init__(self):
+        self.s = hld.Transcription()
+    
+    def listen_for_hl(hl_decoder):
+        exhaust = hl_decoder.run()
+        """
+        if exhaust:
+            print("Stopping listening due to: " + exhaust + " detected in: " + "".join(map(str, hl_decoder.composition.values())))
+        else:
+            print("How am I here?")
+        """
+    
+    def activate(self):
+        t = threading.Thread(target=self.listen_for_hl, args=[self.s])
+        t.start()
+        t.join() # blocks until decoder stops
         
 if __name__ == "__main__":
     s = hld.Transcription()
